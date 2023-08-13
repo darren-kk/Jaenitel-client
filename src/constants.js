97adc5a935dc5583670a4e3dbec2b51deb4db5f0
darren-kk <rlawoghks10@gmail.com>
2023-08-13 19:13:51 +0900

    Feat. add constants for commandList

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const commandList = [
+  { commandName: "h", commandValue: "명령어 모음" },
+  { commandName: "[번호] go", commandValue: "[번호] 페이지 이동" },
+  { commandName: "t", commandValue: "초기 화면" },
+  { commandName: "x", commandValue: "로그아웃 / 종료" },
+];
