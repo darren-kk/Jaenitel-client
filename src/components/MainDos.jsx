70ecb0b5ecae09f002424c1756edd59389e77620
darren-kk <rlawoghks10@gmail.com>
2023-08-14 20:53:20 +0900

    Fix. fix component naming

--- src/components/MainDos.jsx
+++ src/components/MainDos.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { commandList } from "../constants";
 import Input from "./shared/Input";
 
-function Dos() {
+function MainDos() {
   const [command, setCommand] = useState("");
   const [showCommandList, setShowCommandList] = useState(false);
 
@@ -20,12 +20,12 @@ function Dos() {
   }
 
   return (
-    <div className="fixed bottom-0 left-0 w-full">
+    <div className="fixed bottom-0 left-0 bg-blue-bg w-full min-h-2/5">
       <div className="bg-white w-full h-1 mb-2"></div>
       <div className="flex flex-col px-16 py-5">
-        <span className="text-white">명령어 안내(h) 이동(번호/go) 초기화면(t) 종료(x)</span>
+        <span>명령어 안내(h) 이동(번호/go) 초기화면(t) 종료(x)</span>
         <div className="flex">
-          <label className="text-white">
+          <label>
             선택 {">>"}
             <Input
               className="ml-2 outline-none"
@@ -42,7 +42,7 @@ function Dos() {
         </div>
         {showCommandList
           ? commandList.map((item) => (
-              <pre className="text-white" key={item.commandName}>
+              <pre key={item.commandName}>
                 {item.commandName}: {item.commandValue}
               </pre>
             ))
@@ -52,4 +52,4 @@ function Dos() {
   );
 }
 
-export default Dos;
+export default MainDos;
