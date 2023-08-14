963d802501e120ab5af5c5e6b1f70c904967e554
darren-kk <rlawoghks10@gmail.com>
2023-08-14 20:52:49 +0900

    Feat. add routes to main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Dos from "./components/Dos";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import MainDos from "./components/MainDos";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="bg-blue-bg w-screen h-screen">
-      <Dos />
+      <MainDos />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate replace to="/login" />} />
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./main.css";
+
+import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />, //{" "}
   </React.StrictMode>,
 );
