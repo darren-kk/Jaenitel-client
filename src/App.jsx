345feee40b8080bf7017a7c5675c8a8b8714ce03
darren-kk <rlawoghks10@gmail.com>
2023-08-18 05:28:28 +0900

    Feat. add routes for boards

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import MainDos from "./components/MainDos";
 import Login from "./pages/Login";
 import Boards from "./pages/Boards";
+import Board from "./pages/Board";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/boards" element={<Boards />} />
+        <Route path="/boards/greetings" element={<Board />} />
+        <Route path="/boards/free" element={<Board />} />
+        <Route path="/boards/humor" element={<Board />} />
+        <Route path="/boards/special" element={<Board />} />
         <Route path="/" element={<Navigate replace to="/login" />} />
       </Routes>
     </div>
