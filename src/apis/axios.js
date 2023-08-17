02d504513c7c736bbd70b3719db98c5e9dfe756e
darren-kk <rlawoghks10@gmail.com>
2023-08-18 04:19:39 +0900

    Feat. add headers property on axios

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 const baseURL = import.meta.env.VITE_BASE_URL;
 
-function fetchData(method, url, data) {
+function fetchData(method, url, data, headers = { "Content-Type": "application/json" }) {
   const response = axios({
     method,
     url,
     data,
     baseURL,
+    headers,
     withCredentials: true,
   });
 
