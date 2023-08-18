a2aa89624bd480b82060e2a7888f71d572b721ce
darren-kk <rlawoghks10@gmail.com>
2023-08-19 02:34:37 +0900

    Feat. add atom for pagination

--- src/atoms.js
+++ src/atoms.js
@@ -3,3 +3,5 @@ import { atom } from "jotai";
 export const showModalAtom = atom(true);
 export const isSignupAtom = atom(false);
 export const userAtom = atom(null);
+export const currentPageAtom = atom(1);
+export const totalPageAtom = atom(0);
