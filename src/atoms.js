9899b23d40af7b2915319a411235e286824cb55d
darren-kk <rlawoghks10@gmail.com>
2023-08-19 18:53:34 +0900

    Feat. add atoms

--- src/atoms.js
+++ src/atoms.js
@@ -3,5 +3,9 @@ import { atom } from "jotai";
 export const showModalAtom = atom(true);
 export const isSignupAtom = atom(false);
 export const userAtom = atom(null);
+
 export const currentPageAtom = atom(1);
 export const totalPageAtom = atom(0);
+export const postsPerPageAtom = atom(10);
+
+export const scrollRefAtom = atom(null);
