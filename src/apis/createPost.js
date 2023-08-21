7e38d3b0b0cbcd6111504e9a3440a1cfac200134
darren-kk <rlawoghks10@gmail.com>
2023-08-21 20:54:18 +0900

    Feat. add create post api function

---
+++ src/apis/createPost.js
@@ -0,0 +1,54 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { useAtomValue, useSetAtom } from "jotai";
+
+import fetchData from "./axios";
+
+import { userAtom, isNewPostAtom } from "../atoms";
+
+function useCreatePost(postInfo) {
+  console.log(postInfo, "<<<<<");
+  const navigate = useNavigate();
+  const user = useAtomValue(userAtom);
+  const setIsNewPost = useSetAtom(isNewPostAtom);
+
+  async function handleFetchPost() {
+    const formData = new FormData();
+    formData.append("title", postInfo.title);
+    formData.append("category", postInfo.category);
+    formData.append("madeBy", postInfo.madeBy);
+
+    postInfo.contents.forEach((content, index) => {
+      if (content.textContent) {
+        formData.append(`contents[${index}][textContent]`, content.textContent);
+      }
+      if (content.imageContent) {
+        formData.append(`contents[${index}][imageContent]`, content.imageContent);
+      }
+      if (content.videoContent) {
+        formData.append(`contents[${index}][videoContent]`, content.videoContent);
+      }
+    });
+
+    await fetchData("POST", `/users/${user._id}/posts`, formData, {
+      "Content-Type": "multipart/form-data",
+    });
+  }
+
+  const { mutateAsync: fetchPost } = useMutation(handleFetchPost, {
+    onSuccess: () => {
+      setIsNewPost(false);
+      navigate(`/boards/${postInfo.category}`);
+    },
+    onError: (result) => {
+      const error = new Error(result.response.data.message);
+      error.status = result.response.status;
+
+      throw error;
+    },
+  });
+
+  return fetchPost;
+}
+
+export default useCreatePost;
