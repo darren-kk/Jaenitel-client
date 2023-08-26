92bc79d45e2d8acc3612dbfec20277c0b6106c84
darren-kk <rlawoghks10@gmail.com>
2023-08-26 20:24:25 +0900

    Feat. add chatroom components

---
+++ src/pages/ChatRoom.jsx
@@ -0,0 +1,131 @@
+import { useEffect, useRef } from "react";
+import { useQueryClient, useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { useAtomValue, useSetAtom } from "jotai";
+import io from "socket.io-client";
+
+import ChatRoomDos from "../components/ChatRoomDos";
+
+import fetchData from "../apis/axios";
+import { userAtom, scrollRefAtom } from "../atoms";
+
+function ChatRoom() {
+  const scrollRef = useRef(null);
+  const queryClient = useQueryClient();
+  const { roomId } = useParams();
+
+  const setScrollRef = useSetAtom(scrollRefAtom);
+
+  const user = useAtomValue(userAtom);
+
+  async function fetchGetChatRoom() {
+    const data = await fetchData("GET", `/users/${user._id}/chat-rooms/${roomId}`);
+
+    return data.data.chatRoom;
+  }
+
+  const { data: chatRoom } = useQuery(["chatRoom", roomId], fetchGetChatRoom, {
+    keepPreviousData: true,
+    onError: (result) => {
+      const error = new Error(result.response.data.message);
+      error.status = result.response.status;
+
+      throw error;
+    },
+  });
+
+  useEffect(() => {
+    if (scrollRef) {
+      setScrollRef(scrollRef);
+    }
+  }, [setScrollRef]);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+
+    socket.emit("join-room", {
+      roomId: roomId,
+      writer: { _id: user._id, nickname: user.nickname },
+      content: `${user.nickname}님이 입장하셨습니다.`,
+      isSystem: true,
+    });
+
+    socket.on("user-joined", (message) => {
+      queryClient.setQueryData(["chatRoom", roomId], (oldData) => {
+        return {
+          ...oldData,
+          users: [...oldData.users, message.writer],
+          chats: [...oldData.chats, message],
+        };
+      });
+    });
+
+    socket.on("user-left", (message) => {
+      queryClient.setQueryData(["chatRoom", roomId], (oldData) => {
+        return {
+          ...oldData,
+          chats: [...oldData.chats, message],
+        };
+      });
+    });
+
+    socket.on("new-chat", (newChat) => {
+      if (newChat.writer._id === user._id) {
+        return;
+      }
+
+      queryClient.setQueryData(["chatRoom", roomId], (oldData) => {
+        return {
+          ...oldData,
+          chats: [...oldData.chats, newChat],
+        };
+      });
+    });
+
+    return () => {
+      socket.emit("leave-room", {
+        roomId: roomId,
+        writer: { _id: user._id, nickname: user.nickname },
+        content: `${user.nickname}님이 퇴장하셨습니다.`,
+        isSystem: true,
+      });
+      socket.off("new-chat");
+      socket.disconnect();
+    };
+  }, [roomId, queryClient, user._id, user.nickname, user]);
+
+  return (
+    <div className="flex-center pt-5">
+      <header className="flex-center w-full h-20vh pt-5">
+        <div className="flex-center border-menu shadow-lg text-4xl w-4/5 mb-6">실시간 대화방</div>
+        <div className="flex-items-start w-full pl-4">
+          <span className="text-lg w-5/12">방 이름: {chatRoom?.title}</span>
+          <div className="flex w-full justify-start">
+            <span className="text-lg w-1/12 mr-8">참여인원: {chatRoom?.users.length}</span>
+            <div className="text-lg">
+              참여:
+              {chatRoom?.users.map((user) => (
+                <span key={user._id} className="text-lg w-5/12 mx-2">
+                  {user.nickname}
+                </span>
+              ))}
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="bg-white w-full h-1 mb-2"></div>
+      <main ref={scrollRef} className="flex flex-col justify-end items-start w-full h-65vh px-4 py-4 overflow-auto">
+        {chatRoom?.chats?.map((chat) => {
+          return (
+            <pre className="font-dung-guen-mo max-h-40vh mb-2" key={chat._id}>
+              {">>"} {chat.isSystem ? "SYSTEM" : chat.writer?.nickname}: {chat.content}
+            </pre>
+          );
+        })}
+      </main>
+      <ChatRoomDos />
+    </div>
+  );
+}
+
+export default ChatRoom;
---
+++ src/pages/ChatRooms.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useRef } from "react";
+import { useAtom } from "jotai";
+
+import useGetChatRooms from "../apis/getChatrooms";
+
+import { currentPageAtom, totalPageAtom, postsPerPageAtom } from "../atoms";
+
+function ChatRooms() {
+  const [currentPage] = useAtom(currentPageAtom);
+  const [totalPage] = useAtom(totalPageAtom);
+  const [postsPerPage, setPostsPerPage] = useAtom(postsPerPageAtom);
+
+  const mainRef = useRef(null);
+
+  const { chatRooms, totalChatRooms } = useGetChatRooms(currentPage, postsPerPage);
+  const totalUsers = chatRooms?.reduce((acc, chatRoom) => acc + chatRoom.users.length, 0) || 0;
+
+  const pageNumbers = [];
+
+  useEffect(() => {
+    const postHeight = 90;
+
+    const handleResize = () => {
+      if (mainRef.current) {
+        const mainHeight = mainRef.current.clientHeight;
+        setPostsPerPage(Math.floor(mainHeight / postHeight));
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [mainRef, setPostsPerPage]);
+
+  for (let i = 1; i <= Math.ceil(chatRooms?.length / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className="flex-center pt-5">
+      <header className="flex-center w-full h-20vh pt-5">
+        <div className="flex-center border-menu shadow-lg text-4xl w-4/5 mb-12">실시간 대화방</div>
+        <div className="flex justify-start w-full pl-8 pb-2">
+          <span className="text-xl w-2/12">대화방</span>
+          <span className="text-xl w-3/12">개설 방 수: {totalChatRooms} / 99</span>
+          <span className="text-xl w-4/12">현재 참여인원: {totalUsers}명</span>
+        </div>
+      </header>
+      <div className="bg-white w-full h-1 mb-2"></div>
+      <main ref={mainRef} className="flex-start w-full h-60vh px-10 py-4">
+        {chatRooms?.map((chatRoom) => {
+          const users = chatRoom.users;
+
+          return (
+            <div key={chatRoom._id} className="flex flex-col justify-between w-full h-16 mb-8">
+              <div className="flex justify-start w-full mb-4">
+                <span className="text-2xl w-1/12"># {chatRoom.index}</span>
+                <span className="text-2xl w-5/12">{chatRoom.title}</span>
+              </div>
+              <div className="flex justify-start w-full">
+                <span className="text-xl w-2/12">개설일: {chatRoom.createdDate.split("T")[0]}</span>
+                <span className="text-xl mr-2">참여인원:</span>
+                {users.map((user, index) => (
+                  <span key={user._id} className="text-xl mr-4">
+                    {index ? "/ " + user.nickname : user.nickname}
+                  </span>
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </main>
+      <footer>
+        <span className="text-xl">
+          {currentPage} / {totalPage}
+        </span>
+      </footer>
+    </div>
+  );
+}
+
+export default ChatRooms;
