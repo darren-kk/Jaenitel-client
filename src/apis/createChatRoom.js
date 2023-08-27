632266a933a8d3771fca9482182428ca75a1df60
darren-kk <rlawoghks10@gmail.com>
2023-08-27 19:09:14 +0900

    Feat. add create Chatroom api

---
+++ src/apis/createChatRoom.js
@@ -0,0 +1,38 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { useAtomValue, useSetAtom } from "jotai";
+
+import fetchData from "./axios";
+
+import { userAtom, showMainDosAtom } from "../atoms";
+
+function usePostChatRoom(title) {
+  const navigate = useNavigate();
+
+  const user = useAtomValue(userAtom);
+  const setShowMainDos = useSetAtom(showMainDosAtom);
+
+  async function handlePostChatRoom() {
+    return await fetchData("POST", `/users/${user._id}/chat-rooms`, { title });
+  }
+
+  const { mutateAsync: fetchPostChatRoom } = useMutation(handlePostChatRoom, {
+    onSuccess: (result) => {
+      const chatRoomId = result.data.chatRoom._id;
+      const chatRoomIndex = result.data.totalChatRooms + 1;
+
+      setShowMainDos(false);
+      navigate(`/boards/chatrooms/${chatRoomIndex}/${chatRoomId}`);
+    },
+    onError: (result) => {
+      const error = new Error(result.response.data.message);
+      error.status = result.response.status;
+
+      throw error;
+    },
+  });
+
+  return fetchPostChatRoom;
+}
+
+export default usePostChatRoom;
