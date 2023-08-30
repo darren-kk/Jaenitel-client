521a05e4ef621cf1badb513535d17cb5a570c920
darren-kk <rlawoghks10@gmail.com>
2023-08-30 14:13:43 +0900

    Fix. fix refetch after check message

--- src/apis/getMessage.js
+++ src/apis/getMessage.js
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useAtomValue } from "jotai";
 
 import fetchData from "./axios";
@@ -7,12 +7,16 @@ import { userAtom } from "../atoms";
 
 function useGetMessage(messageId) {
   const user = useAtomValue(userAtom);
+  const queryClient = useQueryClient();
 
   async function fetchGetMessage() {
     return await fetchData("GET", `/users/${user._id}/messages/${messageId}`);
   }
 
   const queryInfo = useQuery(["message", messageId], fetchGetMessage, {
+    onSuccess: () => {
+      queryClient.refetchQueries(["messages"]);
+    },
     useErrorBoundary: true,
   });
 
